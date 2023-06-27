567bb9cc0b103c75e9ffc5bec1c718471dc28124
shida <a775734947@qq.com>
2023-06-27 15:33:09 +0800

    feat: 增加doc-sit

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "dumi";
 let base = "/shida-ui"
 let publicPath = "/shida-ui/"
 
+if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
+   base = undefined
+   publicPath = undefined
+}
+
 export default defineConfig({
   title: "SHIDA UI",
   mode: "site",
